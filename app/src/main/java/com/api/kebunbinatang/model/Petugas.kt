6b50f0403535e8d58982e2cea58f18714b89ff6a
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-25 17:01:56 +0700

    Create data class Petugas on Petugas.kt

--- app/src/main/java/com/api/kebunbinatang/model/Petugas.kt
+++ app/src/main/java/com/api/kebunbinatang/model/Petugas.kt
@@ -1,2 +1,10 @@
 package com.api.kebunbinatang.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Petugas(
+    val id_petugas: String,
+    val nama_petugas: String,
+    val jabatan: String
+)
