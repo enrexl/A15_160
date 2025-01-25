e4379e2fd832ba05e58e8fe07944ceec2acf4245
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-26 00:45:25 +0700

    create sealed class HomePetugasUiState and class HomePetugasVM on HomePetugasVM.kt

--- app/src/main/java/com/api/kebunbinatang/ui/viewmodel/petugas/HomePetugasVM.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/viewmodel/petugas/HomePetugasVM.kt
@@ -1,2 +1,50 @@
 package com.api.kebunbinatang.ui.viewmodel.petugas
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.api.kebunbinatang.model.Petugas
+import com.api.kebunbinatang.repo.PetugasRepo
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed class HomePetugasUiState{
+    data class Success(val petugas: List<Petugas>) : HomePetugasUiState()
+    object Error: HomePetugasUiState()
+    object Loading: HomePetugasUiState()
+}
+
+class HomePetugasVM (private val petugas: PetugasRepo) : ViewModel(){
+
+    var petugasUiState: HomePetugasUiState by mutableStateOf(HomePetugasUiState.Loading)
+        private set
+
+    fun getPetugas(){
+        viewModelScope.launch{
+            petugasUiState = HomePetugasUiState.Loading
+
+            try {
+                petugasUiState = HomePetugasUiState.Success(petugas.getPetugas().data)
+            }
+            catch (e:IOException){
+                petugasUiState = HomePetugasUiState.Error
+            }
+            catch (e: HttpException){
+                petugasUiState = HomePetugasUiState.Error
+            }
+        }
+    }
+
+    fun deletePetugas(id_petugas: String){
+        viewModelScope.launch{
+            try {
+                petugas.deletePetugas(id_petugas)
+            } catch (e:IOException){
+                petugasUiState = HomePetugasUiState.Error
+            }
+            catch (e: HttpException){
+                petugasUiState = HomePetugasUiState.Error
+        }
+    }
+}
