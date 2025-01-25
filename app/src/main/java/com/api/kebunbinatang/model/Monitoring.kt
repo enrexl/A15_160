c750da71fe4fd633baa555a8acdfef6148f30029
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-25 17:10:12 +0700

    Create data class Monitoring on Monitoring.kt

--- app/src/main/java/com/api/kebunbinatang/model/Monitoring.kt
+++ app/src/main/java/com/api/kebunbinatang/model/Monitoring.kt
@@ -1,2 +1,14 @@
 package com.api.kebunbinatang.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Monitoring(
+    val id_monitoring: String,
+    val id_petugas: String,
+    val id_kandang: String,
+    val tanggal_monitoring: String,
+    val hewan_sakit: Int,
+    val hewan_sehat: Int,
+    val status: String
+)
