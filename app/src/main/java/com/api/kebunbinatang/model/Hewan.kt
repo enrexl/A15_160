90a2efc9744744fb9329e9bd5da49939e93df0b8
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-25 16:56:38 +0700

    Add data class hewan to Hewan.kt

--- app/src/main/java/com/api/kebunbinatang/model/Hewan.kt
+++ app/src/main/java/com/api/kebunbinatang/model/Hewan.kt
@@ -1,2 +1,12 @@
 package com.api.kebunbinatang.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Hewan (
+    val id_hewan: String,
+    val nama_hewan: String,
+    val tipe_pakan: String,
+    val populasi: String,
+    val zona_wilayah: String
+)
