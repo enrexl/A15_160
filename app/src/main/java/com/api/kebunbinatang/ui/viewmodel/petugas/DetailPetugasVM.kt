eeb8ccbb91f95ed95e735cdf9566003dd1877292
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-26 02:41:31 +0700

    Create class DetailPetugasVM on DetailPetugasVM.kt

--- app/src/main/java/com/api/kebunbinatang/ui/viewmodel/petugas/DetailPetugasVM.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/viewmodel/petugas/DetailPetugasVM.kt
@@ -1,2 +1,35 @@
 package com.api.kebunbinatang.ui.viewmodel.petugas
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.api.kebunbinatang.model.Petugas
+import com.api.kebunbinatang.repo.PetugasRepo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+
+sealed class DetailPetugasUiState {
+    data class Success(val petugas: Petugas) : DetailPetugasUiState()
+    object Error : DetailPetugasUiState()
+    object Loading : DetailPetugasUiState()
+}
+
+class DetailPetugasVM(private val petugasR: PetugasRepo) : ViewModel() {
+
+    private val _detailUiState = MutableStateFlow<DetailPetugasUiState>(DetailPetugasUiState.Loading)
+    val detailUiState: StateFlow<DetailPetugasUiState> = _detailUiState.asStateFlow()
+
+    fun getDetailPetugas(id_petugas: String) {
+        viewModelScope.launch {
+            _detailUiState.value = DetailPetugasUiState.Loading
+            try {
+                val petugas = petugasR.getPetugasByID(id_petugas)
+                _detailUiState.value = DetailPetugasUiState.Success(petugas)
+            } catch (e: Exception) {
+                _detailUiState.value = DetailPetugasUiState.Error
+            }
+        }
+    }
+}
