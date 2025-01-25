8a084bbd15887a66807f7d4c78fc97f334756b1f
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-26 02:30:38 +0700

    Use App on MainActivity.kt

--- app/src/main/java/com/api/kebunbinatang/MainActivity.kt
+++ app/src/main/java/com/api/kebunbinatang/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.api.kebunbinatang.ui.theme.KebunBinatangTheme
+import com.api.kebunbinatang.ui.view.hewan.HomeHewanScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +22,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             KebunBinatangTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    App(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KebunBinatangTheme {
-        Greeting("Android")
     }
 }
