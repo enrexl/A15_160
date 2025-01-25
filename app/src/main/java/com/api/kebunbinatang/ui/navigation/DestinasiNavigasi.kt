a7364369ddac5a9ab90b4713a069af7f9dfcdbbc
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-25 19:19:38 +0700

    Routing Navigasi Hewan on DestinasiNavigasi.kt

--- app/src/main/java/com/api/kebunbinatang/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/navigation/DestinasiNavigasi.kt
@@ -1,2 +1,28 @@
 package com.api.kebunbinatang.ui.navigation
 
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+//Hewan
+object DestHomeHewan : DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+object DestDetailHewan : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Hewan"
+    const val id_hewan = "id_hewan"
+    val routeWithArg = "$route/{$id_hewan}"
+}
+object DestInsertHewan : DestinasiNavigasi{
+    override val route = "hewan_entry"
+    override val titleRes = "Insert Hewan"
+}
+object DestUpdHewan : DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Update Hewan"
+    const val id_hewan = "id_hewan"
+    val routeWithArg = "${DestDetailHewan.route}/{$id_hewan}"
+}
