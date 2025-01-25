1ecff0eb85efdb80540c1bfabace9c4fce59bbe0
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-26 02:33:05 +0700

    Add Destinasi Petugas On DestinasiNavigasi.kt

--- app/src/main/java/com/api/kebunbinatang/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/navigation/DestinasiNavigasi.kt
@@ -5,10 +5,37 @@ interface DestinasiNavigasi {
     val titleRes: String
 }
 
+//main Menu
+object  DestMain: DestinasiNavigasi{
+    override val route = "homes"
+    override val titleRes = "Main Menu"
+}
+//Petugas
+object DestHomePetugas : DestinasiNavigasi{
+    override val route = "homep"
+    override val titleRes = "Home Petugas"
+}
+object DestDetailPetugas : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Petugas"
+    const val id_petugas = "id_petugas"
+    val routeWithArg = "$route/{$id_petugas}"
+}
+object DestInsertPetugas : DestinasiNavigasi{
+    override val route = "hewan_entry"
+    override val titleRes = "Insert Petugas"
+}
+object DestUpdPetugas : DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Update Petugas"
+    const val id_petugas = "id_petugas"
+    val routeWithArg = "${DestDetailPetugas.route}/{$id_petugas}"
+}
+
 //Hewan
 object DestHomeHewan : DestinasiNavigasi{
-    override val route = "home"
-    override val titleRes = "Home Mhs"
+    override val route = "homeh"
+    override val titleRes = "Home Hewan"
 }
 object DestDetailHewan : DestinasiNavigasi {
     override val route = "detail"
