5b2d182222a172588cc94c4016a9ab6adc43370b
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-25 17:56:59 +0700

    create NetworkPetugasRepository on PetugasRepo.kt

--- app/src/main/java/com/api/kebunbinatang/repo/PetugasRepo.kt
+++ app/src/main/java/com/api/kebunbinatang/repo/PetugasRepo.kt
@@ -2,6 +2,8 @@ package com.api.kebunbinatang.repo
 
 import com.api.kebunbinatang.model.AllPetugasResponse
 import com.api.kebunbinatang.model.Petugas
+import com.api.kebunbinatang.service.PetugasService
+import java.io.IOException
 
 interface PetugasRepo {
 
@@ -11,3 +13,45 @@ interface PetugasRepo {
     suspend fun updatePetugas(id_petugas: String, petugas: Petugas)
     suspend fun deletePetugas(id_petugas: String)
 }
+
+class NetworkPetugasRepository(
+    private val PetugasApiService : PetugasService)
+    : PetugasRepo{
+
+    override suspend fun getPetugas(): AllPetugasResponse {
+        PetugasApiService.getAllPetugas()
+    }
+
+    override suspend fun getPetugasByID(id_petugas: String): Petugas {
+     return try {
+        PetugasApiService.getPetugasByID(id_petugas).data
+    } catch (e: Exception){
+        println("Error Fetch Petugas by ID: ${e.message}")
+         throw e }
+    }
+
+    override suspend fun insertPetugas(petugas: Petugas) {
+        PetugasApiService.insertPetugas(petugas)
+    }
+
+    override suspend fun updatePetugas(id_petugas: String, petugas: Petugas) {
+        PetugasApiService.updatePetugas(id_petugas,petugas)
+    }
+
+    override suspend fun deletePetugas(id_petugas: String) {
+        try {
+            val response = PetugasApiService.deletePetugas(id_petugas)
+            if (!response.isSuccessful){
+                throw IOException("Failer to delete Petugas. HTTP Status code: " +
+                        "${response.code()}")
+            }
+                else{
+                    response.message()
+                println(response.message())
+            }
+        }
+        catch (e: Exception){
+            throw e
+        }
+    }
+    }
