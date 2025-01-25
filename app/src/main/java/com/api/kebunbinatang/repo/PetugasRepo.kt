bdc1e9196565e7880f1d97630fcf7d2c5bdddf83
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-25 17:28:09 +0700

    Create interface PetugasRepo onPetugasRepo.kt

--- app/src/main/java/com/api/kebunbinatang/repo/PetugasRepo.kt
+++ app/src/main/java/com/api/kebunbinatang/repo/PetugasRepo.kt
@@ -1,2 +1,13 @@
 package com.api.kebunbinatang.repo
 
+import com.api.kebunbinatang.model.AllPetugasResponse
+import com.api.kebunbinatang.model.Petugas
+
+interface PetugasRepo {
+
+    suspend fun getPetugas(): AllPetugasResponse
+    suspend fun getPetugasByID(id_petugas: String): Petugas
+    suspend fun insertPetugas(petugas: Petugas)
+    suspend fun updatePetugas(id_petugas: String, petugas: Petugas)
+    suspend fun deletePetugas(id_petugas: String)
+}
