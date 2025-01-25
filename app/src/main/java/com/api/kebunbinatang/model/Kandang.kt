243cbd51cdd921530df71963382e8436ea0a1960
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-25 17:07:38 +0700

    Create AllKandangResponse on Kandang.kt

--- app/src/main/java/com/api/kebunbinatang/model/Kandang.kt
+++ app/src/main/java/com/api/kebunbinatang/model/Kandang.kt
@@ -9,3 +9,10 @@ data class Kandang(
     val kapasitas: Int,
     val lokasi: String
 )
+
+@Serializable
+data class AllKandangResponse(
+    val status: Boolean,
+    val message: String,
+    val data:List<Kandang>
+)
