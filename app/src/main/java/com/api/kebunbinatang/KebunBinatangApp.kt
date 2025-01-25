406d4833ad36faa70399302766ff6a9a88666842
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-25 18:34:33 +0700

    Memperbaiki typo pada KebunBinatangApp.kt

--- app/src/main/java/com/api/kebunbinatang/KebunBinatangApp.kt
+++ app/src/main/java/com/api/kebunbinatang/KebunBinatangApp.kt
@@ -1,11 +1,13 @@
 package com.api.kebunbinatang
 
 import android.app.Application
+import com.api.kebunbinatang.di.AppContainer
+import com.api.kebunbinatang.di.KebunBinatangContainer
 
-class KebunBinatangApp: Application {
+class KebunBinatangApp: Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
-        container = KebunBinatangContainer
+        container = KebunBinatangContainer()
     }
 }
