4a48fa0c84633a573f7c69c2ac7dda4baf110722
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-25 17:37:01 +0700

    create interface PetugasService on PetugasService.kt

--- app/src/main/java/com/api/kebunbinatang/service/PetugasService.kt
+++ app/src/main/java/com/api/kebunbinatang/service/PetugasService.kt
@@ -1,4 +1,36 @@
 package com.api.kebunbinatang.service
 
+import com.api.kebunbinatang.model.AllPetugasResponse
+import com.api.kebunbinatang.model.Petugas
+import com.api.kebunbinatang.model.PetugasDetailResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
 interface PetugasService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+
+    @GET("petugas/")
+    suspend fun getAllPetugas(): AllPetugasResponse
+
+    @GET("/petugas/{id_petugas}")
+    suspend fun getPetugasByID(@Path("id_petugas") id_petugas: String) : PetugasDetailResponse
+
+    @POST("/petugas/add")
+    suspend fun insertPetugas(@Body petugas: Petugas)
+
+    @PUT("/petugas/{id_petugas}")
+    suspend fun updatePetugas(@Path("id_petugas") id_petugas: String, @Body petugas: Petugas)
+
+    @DELETE("/petugas/{id_petugas}")
+    suspend fun deletePetugas(@Path("id_petugas")id_petugas: String):Response<Void>
+
 }
