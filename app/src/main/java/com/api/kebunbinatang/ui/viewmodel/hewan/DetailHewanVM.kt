93fce0e4c530a0fefa213b3d10df497b157829da
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-28 21:55:41 +0700

    Create class DetailHewanUiState on DetailHewanVM.kt

--- app/src/main/java/com/api/kebunbinatang/ui/viewmodel/hewan/DetailHewanVM.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/viewmodel/hewan/DetailHewanVM.kt
@@ -1,2 +1,65 @@
 package com.api.kebunbinatang.ui.viewmodel.hewan
 
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.api.kebunbinatang.model.Hewan
+import com.api.kebunbinatang.repo.HewanRepo
+import com.api.kebunbinatang.ui.navigation.DestDetailHewan
+import com.api.kebunbinatang.ui.navigation.DestDetailHewan.id_hewan
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+
+sealed class DetailHewanUiState {
+    data class Success(val hewan: Hewan) : DetailHewanUiState()
+    object Error : DetailHewanUiState()
+    object Loading : DetailHewanUiState()
+}
+
+class DetailHewanVM(private val hewan: HewanRepo) : ViewModel() {
+
+    private val _detailUiState = MutableStateFlow<DetailHewanUiState>(DetailHewanUiState.Loading)
+
+
+    var hewanDetailState: DetailHewanUiState by mutableStateOf(DetailHewanUiState.Loading)
+        private set
+
+    init {
+        getDetailHewan(id_hewan)
+    }
+    fun getDetailHewan(id_hewan:String) {
+        viewModelScope.launch {
+            hewanDetailState = DetailHewanUiState.Loading
+            Log.d("DEBUG", "Fetching petugas with ID: $id_hewan")
+            try {
+                val hewan = hewan.getHewanByID(id_hewan)
+                hewanDetailState = DetailHewanUiState.Success(hewan)
+            } catch (e: Exception) {
+                hewanDetailState = DetailHewanUiState.Error
+            }
+        }
+    }
+
+
+
+    fun deleteHewan(id_hewan:String) {
+        viewModelScope.launch {
+            try {
+                hewan.deleteHewan(id_hewan)
+            }catch (e:IOException){
+                DetailHewanUiState.Error
+            }catch (e: HttpException){
+                DetailHewanUiState.Error
+            }
+        }
+    }
+}
