673d2ae3bede7eaacaa553029823be3471e2763f
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-28 21:54:10 +0700

    Create EntryPetugasScreen on AddPetugas.kt

--- app/src/main/java/com/api/kebunbinatang/ui/view/petugas/AddPetugas.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/view/petugas/AddPetugas.kt
@@ -1,2 +1,164 @@
 package com.api.kebunbinatang.ui.view.petugas
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.api.kebunbinatang.ui.customwidget.CostumeTopAppBar
+import com.api.kebunbinatang.ui.navigation.DestInsertPetugas
+import com.api.kebunbinatang.ui.viewmodel.PenyediaVM
+import com.api.kebunbinatang.ui.viewmodel.petugas.AddPetugasVM
+import com.api.kebunbinatang.ui.viewmodel.petugas.InsertUiEvent
+import com.api.kebunbinatang.ui.viewmodel.petugas.InsertUiState
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryPetugasScreen(
+    navigateBack:()-> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: AddPetugasVM = viewModel(factory = PenyediaVM .Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier =modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestInsertPetugas.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onPetugasValueChange = viewModel :: updateInsertPetugasState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPetugas()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onPetugasValueChange: (InsertUiEvent)->Unit,
+    onSaveClick: ()->Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onPetugasValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "SAVE")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){
+    val jabatan = listOf("Keeper", "Dokter Hewan", "Kurator")
+
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertUiEvent.nama_petugas,
+            onValueChange = {onValueChange(insertUiEvent.copy(nama_petugas = it))},
+            label = { Text("Nama Petugas") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.id_petugas,
+            onValueChange = {onValueChange(insertUiEvent.copy(id_petugas = it))},
+            label = { Text("ID Petugas") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            jabatan.forEach { jabatan ->
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    RadioButton(
+                        selected = insertUiEvent.jabatan == jabatan,
+                        onClick = {
+                            onValueChange(insertUiEvent.copy(jabatan = jabatan))
+                        },
+                    )
+                    Text(
+                        text = jabatan,
+                    )
+                }
+            }
+        }
+
+
+        if(enabled){
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
