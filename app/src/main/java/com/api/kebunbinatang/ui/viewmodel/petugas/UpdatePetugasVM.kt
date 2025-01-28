89c6588d6b85b7ffb6c4c54cf1d2b6d74f362ddc
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-28 22:10:35 +0700

    Add function loadPetugas on UpdatePetugasVM.kt

--- app/src/main/java/com/api/kebunbinatang/ui/viewmodel/petugas/UpdatePetugasVM.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/viewmodel/petugas/UpdatePetugasVM.kt
@@ -1,2 +1,50 @@
 package com.api.kebunbinatang.ui.viewmodel.petugas
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.api.kebunbinatang.model.Petugas
+import com.api.kebunbinatang.repo.PetugasRepo
+import kotlinx.coroutines.launch
+
+
+class UpdatePetugasVM(private val petugasR: PetugasRepo) : ViewModel() {
+
+    var uiState by mutableStateOf(UpdateUiState())
+        private set
+
+
+    fun loadPetugas(id_petugas: String) {
+        viewModelScope.launch {
+            try {
+                val petugas = petugasR.getPetugasByID(id_petugas)
+                uiState = UpdateUiState(insertUiEvent = petugas.toInsertUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    // Update Mahasiswa data
+    fun updatePetugas() {
+        viewModelScope.launch {
+            try {
+                val id_petugas = uiState.insertUiEvent.id_petugas
+                val petugas = uiState.insertUiEvent.toPetugas()
+                petugasR.updatePetugas(id_petugas,petugas)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = UpdateUiState(insertUiEvent = insertUiEvent)
+    }
+}
+
+data class UpdateUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
