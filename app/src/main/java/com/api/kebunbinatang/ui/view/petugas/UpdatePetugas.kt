3678ea5d4875c6ee911f7486905c2b52b6e6f5f6
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-28 22:08:18 +0700

    Add function UpdatePetugasScreen on UpdatePetugas.kt

--- app/src/main/java/com/api/kebunbinatang/ui/view/petugas/UpdatePetugas.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/view/petugas/UpdatePetugas.kt
@@ -1,2 +1,94 @@
 package com.api.kebunbinatang.ui.view.petugas
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.api.kebunbinatang.ui.customwidget.CostumeTopAppBar
+import com.api.kebunbinatang.ui.navigation.DestUpdPetugas
+import com.api.kebunbinatang.ui.viewmodel.PenyediaVM
+import com.api.kebunbinatang.ui.viewmodel.petugas.InsertUiEvent
+import com.api.kebunbinatang.ui.viewmodel.petugas.UpdatePetugasVM
+import com.api.kebunbinatang.ui.viewmodel.petugas.UpdateUiState
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdatePetugasScreen(
+    id_petugas: String,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdatePetugasVM = viewModel(factory = PenyediaVM .Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    // Load data berdasarkan id_oetugas
+    LaunchedEffect(id_petugas) {
+        viewModel.loadPetugas(id_petugas)
+    }
+
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = DestUpdPetugas.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        UpdateBody(
+            updateUiState = viewModel.uiState,
+            onPetugasValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatePetugas()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun UpdateBody(
+    updateUiState: UpdateUiState,
+    onPetugasValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = updateUiState.insertUiEvent,
+            onValueChange = onPetugasValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
