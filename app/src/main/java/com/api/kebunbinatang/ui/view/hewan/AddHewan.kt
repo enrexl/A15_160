a807fe990e1fc72f4579da8eab41cc2fb0a4f42a
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-28 21:53:28 +0700

    Create EntryHewanScreen on AddHewan.kt

--- app/src/main/java/com/api/kebunbinatang/ui/view/hewan/AddHewan.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/view/hewan/AddHewan.kt
@@ -1,2 +1,204 @@
 package com.api.kebunbinatang.ui.view.hewan
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.api.kebunbinatang.ui.customwidget.CostumeTopAppBar
+import com.api.kebunbinatang.ui.navigation.DestInsertHewan
+import com.api.kebunbinatang.ui.viewmodel.PenyediaVM
+import com.api.kebunbinatang.ui.viewmodel.hewan.AddHewanVM
+import com.api.kebunbinatang.ui.viewmodel.hewan.InsertUiEvent
+import com.api.kebunbinatang.ui.viewmodel.hewan.InsertUiState
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryHewanScreen(
+    navigateBack:()-> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: AddHewanVM = viewModel(factory = PenyediaVM .Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier =modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestInsertHewan.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onHewanValueChange = viewModel :: updateInsertHewanState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertHewan()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onHewanValueChange: (InsertUiEvent)->Unit,
+    onSaveClick: ()->Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onHewanValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "SAVE")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("Karnivora", "Omnivora", "Herbivora")
+    var selectedOption by remember { mutableStateOf(options[1]) }
+
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertUiEvent.nama_hewan,
+            onValueChange = {onValueChange(insertUiEvent.copy(nama_hewan = it))},
+            label = { Text("Nama Hewan") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.id_hewan,
+            onValueChange = {onValueChange(insertUiEvent.copy(id_hewan = it))},
+            label = { Text("ID Hewan") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {     OutlinedTextField(
+            value = selectedOption,
+            onValueChange = { },
+            trailingIcon = {
+                    Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = ""
+                    )
+            },
+            label = { Text("Tipe Pakan") },
+            modifier = Modifier.menuAnchor().fillMaxWidth(),
+            readOnly = true,
+            enabled = enabled
+
+        )
+            ExposedDropdownMenu(
+                expanded = expanded,
+            onDismissRequest = { expanded = false }
+            ) {
+                options.forEach { item ->
+                    DropdownMenuItem(onClick = {
+                        selectedOption = item
+                        expanded = false
+                        onValueChange(insertUiEvent.copy(tipe_pakan = item))
+                    },
+                        text = {Text(text = item)}
+                )
+                }
+            }
+        }
+                    OutlinedTextField(
+            value = insertUiEvent.populasi,
+            onValueChange = {onValueChange(insertUiEvent.copy(populasi = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text("Jumlah Populasi") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.zona_wilayah,
+            onValueChange = {onValueChange(insertUiEvent.copy(zona_wilayah = it))},
+            label = { Text("Zona Wilayah") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        if(enabled){
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
