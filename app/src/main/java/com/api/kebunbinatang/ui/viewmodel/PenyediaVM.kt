9afbe0a520594b8d28c0f69e16a1b34571497b42
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-01-28 22:00:44 +0700

    Uncomment PenyediaVM.kt

--- app/src/main/java/com/api/kebunbinatang/ui/viewmodel/PenyediaVM.kt
+++ app/src/main/java/com/api/kebunbinatang/ui/viewmodel/PenyediaVM.kt
@@ -1,23 +1,31 @@
 package com.api.kebunbinatang.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.api.kebunbinatang.KebunBinatangApp
+import com.api.kebunbinatang.ui.viewmodel.hewan.AddHewanVM
+import com.api.kebunbinatang.ui.viewmodel.hewan.DetailHewanVM
 import com.api.kebunbinatang.ui.viewmodel.hewan.HomeHewanViewModel
+import com.api.kebunbinatang.ui.viewmodel.hewan.UpdateHewanVM
+import com.api.kebunbinatang.ui.viewmodel.petugas.AddPetugasVM
+import com.api.kebunbinatang.ui.viewmodel.petugas.DetailPetugasVM
 import com.api.kebunbinatang.ui.viewmodel.petugas.HomePetugasVM
+import com.api.kebunbinatang.ui.viewmodel.petugas.UpdatePetugasVM
 
 object PenyediaVM {
     val Factory = viewModelFactory {
         initializer { HomeHewanViewModel(Kebun().container.hewanRepo) }
-      // initializer { InsertHewanVM(Kebun().container.hewanRepo) }
-      //  initializer { UpdateHewanVM(Kebun().container.hewanRepo) }
-      //  initializer { DelHewanVM(Kebun().container.hewanRepo) }
+       initializer { AddHewanVM(Kebun().container.hewanRepo) }
+        initializer { UpdateHewanVM(Kebun().container.hewanRepo) }
+        initializer { DetailHewanVM(Kebun().container.hewanRepo) }
 
         initializer { HomePetugasVM(Kebun().container.petugasRepo) }
-//        initializer { InsertPetugasVM(Kebun().container.petugasRepo) }
-//        initializer { UpdatePetugasVM(Kebun().container.petugasRepo) }
+        initializer { DetailPetugasVM(Kebun().container.petugasRepo) }
+        initializer { AddPetugasVM(Kebun().container.petugasRepo) }
+        initializer { UpdatePetugasVM(Kebun().container.petugasRepo) }
 //        initializer { DelPetugasVM(Kebun().container.petugasRepo) }
 
 //        initializer { HomeKandangVM(Kebun().container.kandangRepo) }
